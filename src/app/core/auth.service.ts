683b61d629036d9946cff88469a4977c1f952057
damjtoh <damian.crespi@gmail.com>
2018-07-17 21:32:16 -0300

    build

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -57,7 +57,6 @@ export class AuthService {
   login(email: string, password: string) {
     return this.httpLogin(email, password)
       .pipe(
-        map((user:User) => ({...user, rol: 'FARMACEUTICO'})),
         map((user: User) => {
           localStorage.setItem('user', JSON.stringify(user));
           this.userSubject.next(user);
